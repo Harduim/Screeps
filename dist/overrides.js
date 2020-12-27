a1ac36a5abeb2f85cfe981417aa5d42e97c3715f
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2020-12-27 18:00:30 -0300

    better logging info

--- dist/overrides.js
+++ dist/overrides.js
@@ -1,3 +1,11 @@
 Creep.prototype.toString = function () {
+    return JSON.stringify(`[${this.name}] Role:${this.memory.role} Room:${this.room.name}`)
+}
+
+Room.prototype.toString = function () {
     return JSON.stringify(this)
+}
+
+Spawn.prototype.toString = function () {
+    return JSON.stringify(`[${this.name}] Room:${this.room.name} Energy:${this.room.energyAvailable}/${this.room.energyCapacityAvailable}`)
 }
