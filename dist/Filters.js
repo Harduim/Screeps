b25dac07c0a85ff0a9abb4e4966b08ac6db9ccac
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2022-05-08 19:05:08 -0300

    a

--- dist/Filters.js
+++
@@ -1,7 +0,0 @@
-function storageFilter(structure) {
-  let aStorages = [STRUCTURE_EXTENSION, STRUCTURE_SPAWN]
-  if (structure.room.energyAvailable === structure.room.energyCapacityAvailable) {
-    aStorages = [STRUCTURE_EXTENSION, STRUCTURE_SPAWN, STRUCTURE_TOWER, STRUCTURE_STORAGE]
-  }
-  return aStorages.includes(structure.structureType) && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
-}
--- dist/Utils.js
+++
@@ -1,5 +0,0 @@
-const run_safer = (callback) => {
-    try { callback() } catch (e) { log(e, LOG_FATAL) }
-}
-
-module.exports = run_safer
